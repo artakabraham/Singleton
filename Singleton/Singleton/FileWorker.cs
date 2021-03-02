c8d241397c682f53d4542fd729163c713f000391
Artak Abrahamyan <abrahamyan@outlook.com>
2021-03-02 12:35:58 +0400

    Fileworker class

---
+++ Singleton/Singleton/FileWorker.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace Singleton
+{
+    public class FileWorker
+    {
+        public string FilePath { get; }
+        public string Text { get; private set; }
+
+        public FileWorker()
+        {
+            FilePath = "test.txt";
+            ReadtextFromFile();
+        }
+
+        public void WriteText(string text)
+        {
+            Text = text;
+        }
+
+        public void Save()
+        {
+            using var writer = new StreamWriter(FilePath, false);
+            writer.WriteLine(Text);
+        }
+
+        private void ReadtextFromFile()
+        {
+            if (!File.Exists(FilePath))
+            {
+                Text = "";
+            }
+            else
+            {
+                using var reader = new StreamReader(FilePath);
+                Text = reader.ReadToEnd();
+            }
+        }
+    }
+}
